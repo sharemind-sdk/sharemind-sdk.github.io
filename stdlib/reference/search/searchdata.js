6a05ed366a42355cbc6cdb4e84cdebad9d9c1e97
Riivo Talviste <riivo.talviste@cyber.ee>
2016-12-21 09:59:45 +0200

    Regenerated stdlib reference documentation with Doxygen 1.8.12 to fix a layout bug

---
+++ stdlib/reference/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghiklmnopqrstuvwz",
+  1: "_s",
+  2: "acdefkmopstv",
+  3: "_abcdefghiklmnopqrstuvwz",
+  4: "acdgiklmpsu",
+  5: "abcdefghiklmnopqrstuvwz",
+  6: "acdefmostv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "groups",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Modules",
+  6: "Pages"
+};
+
